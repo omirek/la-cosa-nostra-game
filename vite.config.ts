d98d9bb702d93ba7631f104030e8f112c24ddfe7
omirek <140943274+omirek@users.noreply.github.com>
2026-02-09 14:43:45 +0000

    GitHub Pages configure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/la-cosa-nostra-game/', // WAŻNE: Tu musi być dokładna nazwa Twojego repozytorium!
 })
